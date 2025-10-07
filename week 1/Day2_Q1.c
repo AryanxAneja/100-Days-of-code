a7b2eef7aa222a0806fa8e26cabe18304c59fe60
AryanxAneja <aryan.29630@stu.upes.ac.in>
2025-10-07 17:18:11 +0530

    Q2

--- week 1/Day2_Q1.c
+++ week 1/Day2_Q1.c
@@ -2,12 +2,11 @@
 #include<stdio.h>
 int main(){
     float l,b;
-    printf("Enter length");
+    printf("Enter length :");
     scanf("%f", &l);
     printf("Enter Breadth");
     scanf("%f",&b);
     printf("Area of Rectangle is : %f \n", l*b);
     printf("Perimeter of Rectangle is : %f \n", 2*(l+b));
     return 0;
-
 }
