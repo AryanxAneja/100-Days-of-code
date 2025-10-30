844cb0adb3a66ba30492dc1a6c30e86a00c73c7f
AryanxAneja <aryan.29630@stu.upes.ac.in>
2025-10-30 11:43:26 +0530

    >

--- Week 3/Day19_Q38.c
+++ Week 3/Day19_Q38.c
@@ -1,1 +1,14 @@
 //Write a program to find the sum of digits of a number.
+#include<stdio.h>
+
+int main(){
+    int num, sum = 0;
+    printf("Enter a number : ");
+    scanf("%d", &num);
+    while(num != 0){
+        sum += num%10;
+        num /= 10;
+    }
+    printf("The sum of digits is : %d",sum);
+    return 0;
+}
