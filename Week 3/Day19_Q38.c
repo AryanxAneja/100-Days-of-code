e31d0e58d5c6995fb39c17e643401611df9f7d10
AryanxAneja <aryan.29630@stu.upes.ac.in>
2025-11-01 20:04:24 +0530

    >

--- Week 3/Day19_Q38.c
+++ Week 3/Day19_Q38.c
@@ -2,12 +2,16 @@
 #include<stdio.h>
 
 int main(){
-    int num, sum = 0;
-    printf("Enter a number : ");
-    scanf("%d", &num);
-    while(num != 0){
-        sum += num%10;
-        num /= 10;
+    int digits = 0;
+    int n;
+    int sum = 0;
+    printf("Enter a Number : ");
+    scanf("%d",&n);
+
+    while(n!=0){
+        digits = n%10;
+        n= n/10;
+        sum = sum + digits;
     }
     printf("The sum of digits is : %d",sum);
     return 0;
