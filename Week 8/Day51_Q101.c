047e6bcb381e10a0b42d54e9b5c8545a3f02e5e1
AryanxAneja <aryan.29630@stu.upes.ac.in>
2025-11-22 22:21:00 +0530

    Day 51

--- Week 8/Day51_Q101.c
+++ Week 8/Day51_Q101.c
@@ -1,14 +1,18 @@
-// Write a Program to take a sorted array(say nums[]) and an integer (say target) as inputs. The elements in the sorted array might be repeated. You need to print the first and last occurrence of the target and print the index of first and last occurrence. Print -1, -1 if the target is not present.
+/*Write a Program to take a sorted array(say nums[]) and an integer (say target) as inputs.
+ The elements in the sorted array might be repeated. You need to print the first and last occurrence of the target and print the index of first and last occurrence. Print -1, -1 if the target is not present.*/
 #include<stdio.h>
 int main(){
     int n;
-    int nums[n];
+    int target;
     printf("Enter no of Elements : ");
     scanf("%d",&n);
+    int nums[n];
     printf("Enter Elements: ");
     for(int i=0;i<n;i++){
         scanf("%d",&nums[i]);
     }
+    printf("Enter the Number you want to find: ");
+    scanf("%d",&target);
     for(int i = 0;i<n;i++){
         for(int j=0;j<n-1-i;j++){
             if(nums[j]>nums[j+1]){
@@ -18,8 +22,21 @@ int main(){
             }
         }
     }
+    int firstOccurence = -1;
+    int lastOccurence = -1;
 
     for(int i=0;i<n;i++){
-        
+        if(nums[i]==target){
+            firstOccurence = i;
+            break;
+        }
     }
+        for(int i=0;i<n;i++){
+        if(nums[i]==target && i!=firstOccurence){
+            lastOccurence = i;
+        }
+        }
+    printf("The First Occurence is at index:%d\n",firstOccurence);
+    printf("The Last Occurence is at index:%d",lastOccurence);
+    return 0;
 }
