2efc0dcba6101c695440f6d3d39e0aac0cafc8ee
AryanxAneja <aryan.29630@stu.upes.ac.in>
2025-11-22 23:25:01 +0530

    Day 55

--- Week 08/Day55_Q105.c
+++ Week 08/Day55_Q105.c
@@ -1,1 +1,24 @@
-Write a program to take an integer array nums of size n, and print the majority element. The majority element is the element that appears strictly more than ⌊n / 2⌋ times. Print -1 if no such element exists. Note: Majority Element is not necessarily the element that is present most number of times.
+// Write a program to take an integer array nums of size n, and print the majority element. The majority element is the element that appears strictly more than ⌊n / 2⌋ times. Print -1 if no such element exists. Note: Majority Element is not necessarily the element that is present most number of times.
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    int nums[n];
+    for(int i = 0; i < n; i++)
+        scanf("%d", &nums[i]);
+    int major = -1;
+    for(int i = 0; i < n; i++) {
+        int count = 0;
+        for(int j = 0; j < n; j++) {
+            if(nums[i] == nums[j]) {
+                count++;
+            }
+        }
+        if(count > n/2) {
+            major = nums[i];
+            break;
+        }
+    }
+    printf("%d", major);
+    return 0;
+}
