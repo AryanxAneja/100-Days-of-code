de89e45595be7056d09d8691b1aae0d0d791b4bd
AryanxAneja <aryan.29630@stu.upes.ac.in>
2025-11-22 23:20:12 +0530

    Day 53 and 54

--- Week 08/Day54_Q104.c
+++ Week 08/Day54_Q104.c
@@ -1,1 +1,23 @@
-Write a Program to take a positive integer n as input, and find the pivot integer x such that the sum of all elements between 1 and x inclusively equals the sum of all elements between x and n inclusively. Print the pivot integer x. If no such integer exists, print -1. Assume that it is guaranteed that there will be at most one pivot integer for the given input.
+// Write a Program to take a positive integer n as input, and find the pivot integer x such that the sum of all elements between 1 and x inclusively equals the sum of all elements between x and n inclusively. Print the pivot integer x. If no such integer exists, print -1. Assume that it is guaranteed that there will be at most one pivot integer for the given input.
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    int pivot = -1;
+    for(int x = 1; x <= n; x++) {
+        int leftSum = 0, rightSum = 0;
+        for(int i = 1; i <= x; i++) {
+            leftSum += i;
+        }
+        for(int i = x; i <= n; i++) {
+            rightSum += i;
+        }
+        if(leftSum == rightSum) {
+            pivot = x;
+            break;
+        }
+    }
+    printf("%d", pivot);
+    return 0;
+}
